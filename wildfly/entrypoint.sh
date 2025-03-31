ba9e223941beaebdea0b7e6565ee8662ae872770
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-31 17:54:03 +0200

    Include console in conf.d endpoint-1b

--- wildfly/entrypoint.sh
+++ wildfly/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-/opt/jboss/wildfly/bin/add-user.sh "$WILDFLY_USER" "$WILDFLY_PASS" --silent || true
+$WILDFLY_HOME/bin/add-user.sh -a -u $WILDFLY_USER -p $WILDFLY_PASS --silent || true
 
-/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
+$WILDFLY_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
 WILDFLY_PID=$!
 
 until curl -s http://localhost:9990/management > /dev/null; do
   sleep 1
 done
 
-/opt/jboss/wildfly/bin/jboss-cli.sh --connect <<EOF
-if (outcome != success) of /core-service=management/access=authorization/role-mapping=SuperUser/include=admin:read-resource
-    /core-service=management/access=authorization/role-mapping=SuperUser/include=admin:add(name="$WILDFLY_USER", type="USER")
+$WILDFLY_HOME/bin/jboss-cli.sh --connect <<EOF
+if (outcome != success) of /core-service=management/access=authorization/role-mapping=SuperUser/include=$WILDFLY_USER:read-resource
+    /core-service=management/access=authorization/role-mapping=SuperUser/include=$WILDFLY_USER:add(name="$WILDFLY_USER", type="USER")
 end-if
 EOF
 
