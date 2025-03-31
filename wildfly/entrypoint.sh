25f248bba6fb83ecc0f1bd2b4b7bfd40716a2a40
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-31 17:31:24 +0200

    Modify entrypoint-1b

--- wildfly/entrypoint.sh
+++ wildfly/entrypoint.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "==> Create admin user if not exist..."
-$WILDFLY_HOME/bin/add-user.sh "$WILDFLY_USER" "$WILDFLY_PASS" --silent || true
+/opt/jboss/wildfly/bin/add-user.sh "$WILDFLY_USER" "$WILDFLY_PASS" --silent || true
 
-echo "==> Starting WildFly in background for manage CLI..."
-$WILDFLY_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
+/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
 WILDFLY_PID=$!
 
-echo "==> Waitting server to start..."
 until curl -s http://localhost:9990/management > /dev/null; do
   sleep 1
 done
 
-echo "==> Asign SuperUser rol to $WILDFLY_USER..."
-$WILDFLY_HOME/bin/jboss-cli.sh --connect --commands="/core-service=management/access=authorization/role-mapping=SuperUser/include=user-admin:add(name=${WILDFLY_USER})" || true
+/opt/jboss/wildfly/bin/jboss-cli.sh --connect --commands="/core-service=management/access=authorization/role-mapping=SuperUser/include=user-admin:add(name=${WILDFLY_USER})" || true
 
-# Traer el servidor al primer plano
-echo "==> Execute WildFly in foreground..."
 wait $WILDFLY_PID
