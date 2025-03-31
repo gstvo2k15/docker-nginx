b25405755be0c6d076abea993c7ccd2272cb84f1
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-31 17:28:29 +0200

    Modify entrypoint

--- wildfly/entrypoint.sh
+++ wildfly/entrypoint.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -e
 
-# Create admin user if already not exist
+echo "==> Create admin user if not exist..."
 $WILDFLY_HOME/bin/add-user.sh "$WILDFLY_USER" "$WILDFLY_PASS" --silent || true
 
-# Execute WildFly (allow mgmt)
-exec $WILDFLY_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
+echo "==> Starting WildFly in background for manage CLI..."
+$WILDFLY_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
+WILDFLY_PID=$!
+
+echo "==> Waitting server to start..."
+until curl -s http://localhost:9990/management > /dev/null; do
+  sleep 1
+done
+
+echo "==> Asign SuperUser rol to $WILDFLY_USER..."
+$WILDFLY_HOME/bin/jboss-cli.sh --connect --commands="/core-service=management/access=authorization/role-mapping=SuperUser/include=user-admin:add(name=${WILDFLY_USER})" || true
+
+# Traer el servidor al primer plano
+echo "==> Execute WildFly in foreground..."
+wait $WILDFLY_PID
