2133de91a14ffd2afa878ac3006f03d974aca865
Gustavo Olmo <gstvo2k15@gmail.com>
2025-03-31 17:57:19 +0200

    Include console in conf.d endpoint-1c

--- wildfly/entrypoint.sh
+++ wildfly/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-$WILDFLY_HOME/bin/add-user.sh -a -u $WILDFLY_USER -p $WILDFLY_PASS --silent || true
+/opt/jboss/wildfly/bin/add-user.sh -a -u "$WILDFLY_USER" -p "$WILDFLY_PASS" --silent || true
 
-$WILDFLY_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
+/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
 WILDFLY_PID=$!
 
 until curl -s http://localhost:9990/management > /dev/null; do
   sleep 1
 done
 
-$WILDFLY_HOME/bin/jboss-cli.sh --connect <<EOF
+/opt/jboss/wildfly/bin/jboss-cli.sh --connect <<EOF
 if (outcome != success) of /core-service=management/access=authorization/role-mapping=SuperUser/include=$WILDFLY_USER:read-resource
     /core-service=management/access=authorization/role-mapping=SuperUser/include=$WILDFLY_USER:add(name="$WILDFLY_USER", type="USER")
 end-if
