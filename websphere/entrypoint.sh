d0f7b1c5ba885581891066427b103382352e7617
Gustavo Olmo <gstvo2k15@gmail.com>
2025-04-01 10:44:53 +0200

    Copy entrypoint properly-1b

--- websphere/entrypoint.sh
+++ websphere/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 set -e
 
-# Create admin user if basicRegistry is used
-if ! grep -q "$ADMIN_USER" "/config/server.xml"
-  then
-    echo "Basic user registry is not configured in server.xml"
-  fi
-
-# Start Liberty
 exec /opt/ibm/wlp/bin/server run "$SERVER_NAME"
